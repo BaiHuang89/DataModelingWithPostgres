203b53ab14fabdf4e4a81dc77742652d548a20bc
MichelleW <michelle.yanru.wang@gmail.com>
2019-06-26 19:55:32 +0800

    Finish the DDL statements.

--- sql_queries.py
+++ sql_queries.py
@@ -1,49 +1,116 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;" # user is a reserved word in PostgreSQL
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id BIGSERIAL PRIMARY KEY,
+        start_time TIMESTAMP NOT NULL,
+        user_id INT NOT NULL,
+        level VARCHAR(10) NOT NULL,
+        song_id VARCHAR(100) NOT NULL,
+        artist_id VARCHAR(100) NOT NULL,
+        session_id INT NOT NULL,
+        location VARCHAR(256),
+        user_agent VARCHAR
+    );
+
+    COMMENT ON COLUMN songplays.songplay_id is 'AutoIncrement Column';
+    COMMENT ON COLUMN songplays.user_agent is 'No Specified limit for Headers according to HTTP specification. Web servers do have the limit and vary.';
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INT PRIMARY KEY,
+        first_name VARCHAR(100) NOT NULL,
+        last_name VARCHAR(100) NOT NULL,
+        gender VARCHAR(1) NOT NULL,
+        level VARCHAR(20) NOT NULL
+    );
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id VARCHAR(100) PRIMARY KEY,
+        title VARCHAR(256) NOT NULL,
+        artist_id VARCHAR(100) NOT NULL,
+        year SMALLINT,
+        duration NUMERIC
+    );
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id VARCHAR(100) PRIMARY KEY,
+        name VARCHAR(256) NOT NULL,
+        location VARCHAR(256),
+        latitude NUMERIC,
+        longitude NUMERIC
+    );
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIMESTAMP PRIMARY KEY,
+        hour SMALLINT NOT NULL,
+        day SMALLINT NOT NULL,
+        week SMALLINT NOT NULL,
+        month SMALLINT NOT NULL,
+        year SMALLINT NOT NULL,
+        weekday SMALLINT NOT NULL
+    );
+
+    COMMENT ON COLUMN time.start_time is 'The value of log.ts';
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+    INSERT INTO users(user_id, first_name, last_name, gender, level)
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id)
+    DO NOTHING;
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs(song_id, title, artist_id, year, duration) 
+    VALUES(%s, %s, %s, %s, %s) 
+    ON CONFLICT (song_id) 
+    DO NOTHING;
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists(artist_id, name, location, latitude, longitude) 
+    VALUES(%s, %s, %s, %s, %s) 
+    ON CONFLICT (artist_id) 
+    DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time(start_time, hour, day, week, month, year, weekday) 
+    VALUES(%s, %s, %s, %s, %s, %s, %s) 
+    ON CONFLICT (start_time) 
+    DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT songs.song_id, artists.artist_id 
+    FROM (songs JOIN artists ON songs.artist_id = artists.artist_id)
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
 """)
 
 # QUERY LISTS
